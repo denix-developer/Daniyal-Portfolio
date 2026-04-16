1f1969b0cf2b35187cc3b0140433ff8366234f2d
Muhammad Daniyal <102672505+denix-developer@users.noreply.github.com>
2026-04-16 21:56:19 +0500

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,12 +11,12 @@ const Contact = () => {
             <h4>Connect</h4>
             <p>
               <a
-                href="https://www.linkedin.com/in/akashrmalhotra/"
+                href="https://www.linkedin.com/in/muhammaddaniyalll/"
                 target="_blank"
                 rel="noreferrer"
                 data-cursor="disable"
               >
-                LinkedIn — akashrmalhotra
+                LinkedIn — muhammaddaniyalll
               </a>
             </p>
             <h4>Education</h4>
@@ -31,7 +31,7 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Social</h4>
             <a
-              href="https://github.com/akashrmalhotra"
+              href="https://github.com/denix-developer"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -40,7 +40,7 @@ const Contact = () => {
               GitHub <MdArrowOutward />
             </a>
             <a
-              href="https://www.linkedin.com/in/akashrmalhotra/"
+              href="https://www.linkedin.com/in/muhammaddaniyalll/"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -48,7 +48,7 @@ const Contact = () => {
             >
               LinkedIn <MdArrowOutward />
             </a>
-            <a
+            /*{ <a
               href="https://www.youtube.com/@Leftbraincoder"
               target="_blank"
               rel="noreferrer"
@@ -56,9 +56,9 @@ const Contact = () => {
               className="contact-social"
             >
               YouTube <MdArrowOutward />
-            </a>
-            <a
-              href="https://www.instagram.com/leftbraincoder/"
+            </a>*/
+            <a }
+              href="https://www.instagram.com/daniyyawl2.0/"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -69,7 +69,7 @@ const Contact = () => {
           </div>
           <div className="contact-box">
             <h2>
-              Designed and Developed <br /> by <span>Akash Malhotra</span>
+              Designed and Developed <br /> by <span>Muhammad Daniyal</span>
             </h2>
             <h5>
               <MdCopyright /> 2026
